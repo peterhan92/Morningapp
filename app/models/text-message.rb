01842863621f517e569c0150e3638485d82154d6
Peter Han <han.peter92@gmail.com>
2015-10-10 10:08:22 -0400

    small edit

--- app/models/text-message.rb
+++ app/models/text-message.rb
@@ -1,6 +1,6 @@
 class Textmessage
 	
-	@keys = YAML.load_file('/twilio-sid-token.yml')
+	@keys = YAML.load_file('')
 	account_sid = @keys['ACCOUNT_SID']
 	auth_token = @keys['AUTH_TOKEN']
 	@client = Twilio::REST::Client.new account_sid, auth_token
@@ -9,14 +9,10 @@ class Textmessage
 	    puts "Sending a message to #{number} : \'#{msg}\'"
 
 	    @client.account.messages.create( 
-	    :from => '8455354138',
+	    :from => ,
 	    :to => number,
 	    :body => msg
 	    )
 	end
 
 end
-
-test = Textmessage.new
-binding.pry
-test.send_msg(334,"hu")
