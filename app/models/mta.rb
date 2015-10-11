f9a24de78167401d9654fe900984a39520481d0e
Peter Han <han.peter92@gmail.com>
2015-10-11 13:26:29 -0400

    small changes

--- app/models/mta.rb
+++ app/models/mta.rb
@@ -1,20 +1,3 @@
 class Mta
-	def initialize
-		url = "http://www.mta.info/"
-		@mta = Nokogiri::HTML(open(url))
-	end
 
-	def subway
-		@subway = @mta.css('div.span-11 img.subwayIcon_7')
-	end
-
-	def status
-		@status = @mta.css('div.subwayCategory#123 a')
-		# puts @status
-	end
-
-	def time
-		@time = @mta.css('span.statusDateTime').text
-		puts @time
-	end
 end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,11 +1,11 @@
 class Weather
-	def initialize
-		url = "http://www.weather.com/weather/today/l/USNY0996:1:US"
-		@weather = Nokogiri::HTML(open(url))
-	end
+	# def initialize
+	# 	url = "http://www.weather.com/weather/today/l/USNY0996:1:US"
+	# 	@weather = Nokogiri::HTML(open(url))
+	# end
 
-	def temp
-		@degrees = @weather.css('div.temperature')
-		puts @degrees
-	end
+	# def temp
+	# 	@degrees = @weather.css('div.temperature')
+	# 	puts @degrees
+	# end
 end
