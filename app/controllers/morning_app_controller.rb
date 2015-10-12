5a9b7189f756433bc840367fc020822ddfd87df7
Peter Han <han.peter92@gmail.com>
2015-10-12 13:30:57 -0400

    fix merge conflict

--- app/controllers/morning_app_controller.rb
+++ app/controllers/morning_app_controller.rb
@@ -6,17 +6,12 @@ class MorningAppController < ApplicationController
 
 	def text
 		@phone_number = params["number"]
-<<<<<<< HEAD
-		Phonenumber.create(:number => params["number"])
-		# Textmessage.new(params["number"], "hey")
-=======
 		valid = Validate.new(@phone_number.to_s).phone_number?
 		if valid 
 			Phonenumber.create(:number => params["number"])
 		else
 			flash[:success] = "<b>Please Enter Valid Phone Number</b>" 
 		end
->>>>>>> mta
 		render('/morning_app/text')
 	end
 end
