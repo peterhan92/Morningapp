997d849c171cd7a1c618b40d010899cccbedf14d
Peter Han <han.peter92@gmail.com>
2015-10-12 20:27:59 -0400

    add more sessions to controllers

--- app/controllers/morning_app_controller.rb
+++ app/controllers/morning_app_controller.rb
@@ -1,11 +1,19 @@
 class MorningAppController < ApplicationController
 	def main
 		@wotd = Wordoftheday.new
-		@wotd_display = "#{@wotd.word} - #{@wotd.definition}"
-		@mta = Mta.new.subway_status
+		@wotd_text = "#{@wotd.word} - #{@wotd.definition}"
+
+		@mta = Mta.new
+
 		@quote = Quote.new
+		@quote_text = "\"#{@quote.content}\" - #{@quote.person}"
+
 		@weather = Nycweather.new
-		session['wotd_disp'] = @wotd_display
+		@weather_text = "#{@weather.location}: #{@weather.sky} #{@weather.temperature}"
+
+		session['wotd'] = @wotd_text
+		session['quote'] = @quote
+		# session['weather'] = @weather
 	end
 
 	def text
@@ -13,7 +21,7 @@ class MorningAppController < ApplicationController
 		valid = Validate.new(@phone_number.to_s).phone_number?
 		if valid 
 			Phonenumber.create(:number => params["number"])
-			Textmessage.new(params["number"], session['wotd_disp'])
+			Textmessage.new(params["number"], session['wotd'])
 			render('/morning_app/text')
 		else
 			flash[:success] = "<b>Please Enter Valid Phone Number</b>"
