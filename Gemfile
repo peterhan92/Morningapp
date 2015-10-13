5c3b77613ba732b510e6fceeeae4eae7aace0434
Peter Han <han.peter92@gmail.com>
2015-10-12 22:38:19 -0400

    changed mta array to hash and messed with sessions

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'json'
 # nokogiri
 gem 'nokogiri'
 
+# gem 'activerecord-session_store'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/morning_app_controller.rb
+++ app/controllers/morning_app_controller.rb
@@ -1,19 +1,22 @@
 class MorningAppController < ApplicationController
 	def main
 		@wotd = Wordoftheday.new
-		@wotd_text = "#{@wotd.word} - #{@wotd.definition}"
+		@wotd_text = "Word of the day: #{@wotd.word} - #{@wotd.definition}"
 
 		@mta = Mta.new
+		@mta_text = @mta.subway_status
 
 		@quote = Quote.new
-		@quote_text = "\"#{@quote.content}\" - #{@quote.person}"
+		@quote_text = "Quote: \"#{@quote.content}\" - #{@quote.person}"
 
 		@weather = Nycweather.new
-		@weather_text = "#{@weather.location}: #{@weather.sky} #{@weather.temperature}"
+		@weather_text = "Weather in #{@weather.location}: #{@weather.sky} #{@weather.temperature}"
 
 		session['wotd'] = @wotd_text
-		session['quote'] = @quote
-		# session['weather'] = @weather
+		session['quote'] = @quote_text
+		session['weather'] = @weather_text
+
+		session['mta'] = @mta.subway_status
 	end
 
 	def text
@@ -21,7 +24,9 @@ class MorningAppController < ApplicationController
 		valid = Validate.new(@phone_number.to_s).phone_number?
 		if valid 
 			Phonenumber.create(:number => params["number"])
-			Textmessage.new(params["number"], session['wotd'])
+			Textmessage.new(params["number"], "*** #{session['wotd']} *** #{session['quote']} *** #{session['weather']} *** #{session['mta']}")
+			# Textmessage.new(params["number"], session['quote'])
+			# Textmessage.new(params["number"], session['weather'])
 			render('/morning_app/text')
 		else
 			flash[:success] = "<b>Please Enter Valid Phone Number</b>"
